913830e1411b847b15e635624f16cd8441d1f5ff
paulkennethkent <pkennethkent@gmail.com>
2017-10-04 12:02:27 +0100

    moving making calc engine maven

---
+++ src/main/java/com/diffblue/calcengine/App.java
@@ -0,0 +1,30 @@
+package com.diffblue.calcengine;
+
+public class App {
+
+    public static void main(String[] args) {
+        MathEquation[] equations = new MathEquation[4];
+        equations[0] = create(100.0d, 50.d, 'd');
+        equations[1] = create(25.0d, 92.0d, 'a');
+        equations[2] = create(225.0d, 17.0d, 'm');
+        equations[3] = create(11.0d, 3.0d, 'm');
+
+
+        for (MathEquation equation : equations) {
+            equation.execute();
+            System.out.print("result = ");
+            System.out.println(equation.result);
+        }
+    }
+
+    public static MathEquation create(double leftVal, double rightVal, char opCode) {
+        MathEquation equation = new MathEquation();
+        equation.leftVal = leftVal;
+        equation.rightVal = rightVal;
+        equation.opCode = opCode;
+
+        return equation;
+
+    }
+
+}
---
+++ src/main/java/com/diffblue/calcengine/MathEquation.java
@@ -0,0 +1,33 @@
+package com.diffblue.calcengine;
+
+public class MathEquation {
+    public double leftVal;
+    public double rightVal;
+    public char opCode;
+    public double result;
+
+    public void execute() {
+        switch (opCode) {
+            case 'a':
+                result = leftVal + rightVal;
+                break;
+            case 's':
+                result = leftVal - rightVal;
+                break;
+            case 'd':
+                result = rightVal != 0.0d ? leftVal / rightVal : 0.0d;
+                break;
+            case 'm':
+                result = leftVal * rightVal;
+                break;
+            default:
+                System.out.println("Error - invalid opcode");
+                result = 0.0d;
+                break;
+        }
+
+
+    }
+
+
+}
