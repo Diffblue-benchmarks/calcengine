e46a984dbba71396da829de5de4b96923857d434
paulkennethkent <pkennethkent@gmail.com>
2017-10-04 14:51:39 +0100

    accessors and mutators (getters and setters)

--- src/main/java/com/diffblue/calcengine/MathEquation.java
+++ src/main/java/com/diffblue/calcengine/MathEquation.java
@@ -1,10 +1,18 @@
 package com.diffblue.calcengine;
 
 public class MathEquation {
-    public double leftVal;
-    public double rightVal;
-    public char opCode;
-    public double result;
+    private double leftVal;
+    private double rightVal;
+    private char opCode;
+    private double result;
+
+    public double getLeftVal() { return leftVal; }
+    public void setLeftVal(double leftVal) { this.leftVal = leftVal; }
+    public double getRightVal() { return rightVal; }
+    public void setRightVal(double rightVal) { this.rightVal = rightVal; }
+    public char getOpCode() { return opCode; }
+    public void setOpCode(char opCode) { this.opCode = opCode; }
+    public double getResult() { return result; }
 
     public void execute() {
         switch (opCode) {
