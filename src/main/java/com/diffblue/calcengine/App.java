1d3db353affc1f32c1fe37064786bd5b20f21589
paulkennethkent <pkennethkent@gmail.com>
2017-10-06 13:28:39 +0100

    overloading methods and contructors

--- src/main/java/com/diffblue/calcengine/App.java
+++ src/main/java/com/diffblue/calcengine/App.java
@@ -15,6 +15,36 @@ public class App {
             System.out.print("result = ");
             System.out.println(equation.getResult());
         }
+
+        System.out.println();
+        System.out.println("Using Overloads");
+        System.out.println();
+
+
+        double leftDouble = 9.0d;
+        double rightDouble = 4.0d;
+
+        int leftInt = 9;
+        int rightInt = 4;
+
+
+        MathEquation equationOverload = new MathEquation('d');
+
+        equationOverload.execute(leftDouble, rightDouble);
+        System.out.println("result double=");
+        System.out.println(equationOverload.getResult());
+
+
+        equationOverload.execute(leftInt,rightInt );
+        System.out.println("result int=");
+        System.out.println(equationOverload.getResult());
+
+        equationOverload.execute(leftInt,rightDouble );
+        System.out.println("result int and doubs=");
+        System.out.println(equationOverload.getResult());
+
+
+
     }
 
 }
--- src/main/java/com/diffblue/calcengine/MathEquation.java
+++ src/main/java/com/diffblue/calcengine/MathEquation.java
@@ -27,6 +27,22 @@ public class MathEquation {
         this.rightVal = rightVal;
     }
 
+    public void execute(double leftVal, double rightVal) {
+        this.leftVal = leftVal;
+        this.rightVal = rightVal;
+
+        execute();
+    }
+
+    public void execute(int leftVal, int rightVal) {
+        this.leftVal = leftVal;
+        this.rightVal = rightVal;
+
+        execute();
+
+        result = (int)result;
+    }
+
     public void execute() {
         switch (opCode) {
             case 'a':
